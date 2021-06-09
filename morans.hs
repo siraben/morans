7251b389e04e292f9f1432439b3f38fa09512b61
Ben Siraphob <bensiraphob@gmail.com>
2021-06-09 14:28:19 +0700

    Reindent

--- morans.hs
+++ morans.hs
@@ -1,23 +1,24 @@
 {-# LANGUAGE ViewPatterns #-}
+
 -- Requires files from http://yann.lecun.com/exdb/mnist/
-import           Codec.Compression.GZip         ( decompress )
-import qualified Data.ByteString.Lazy          as BS
-import           System.Environment
-import           System.Exit
-import           System.IO
-import           System.Random
-import           Data.Ord
-import           GHC.Int                        ( Int64 )
-
-import           Control.Monad
-import           Data.List
-import           Data.Foldable                  ( for_ )
-
-import qualified Data.Vector as V
+import Codec.Compression.GZip (decompress)
+import Control.Monad
+import qualified Data.ByteString.Lazy as BS
+import Data.Foldable (for_)
+import Data.List
+import Data.Ord
 import Data.Vector (Vector)
+import qualified Data.Vector as V
+import GHC.Int (Int64)
+import System.Environment
+import System.Exit
+import System.IO
+import System.Random
 
 type Biases = Vector Double
+
 type Weights = Vector (Vector Double)
+
 type NeuralNet = Vector (Biases, Weights)
 
 gauss :: Double -> IO Double
@@ -27,7 +28,8 @@ gauss scale = do
   return $ scale * sqrt (-2 * log x1) * cos (2 * pi * x2)
 
 newBrain :: Vector Int -> IO NeuralNet
-newBrain szs@(V.uncons -> Just (_ , ts)) = V.zip (flip V.replicate 1 <$> ts)
+newBrain szs@(V.uncons -> Just (_, ts)) =
+  V.zip (flip V.replicate 1 <$> ts)
     <$> V.zipWithM (\m n -> V.replicateM n $ V.replicateM m $ gauss 0.01) szs ts
 
 -- activation function
@@ -36,8 +38,9 @@ relu = max 0
 
 -- derivative of activation function
 relu' :: (Ord a, Num a, Num p) => a -> p
-relu' x | x < 0     = 0
-        | otherwise = 1
+relu' x
+  | x < 0 = 0
+  | otherwise = 1
 
 zLayer :: Vector Double -> (Vector Double, Vector (Vector Double)) -> Vector Double
 zLayer as (bs, wvs) = V.zipWith (+) bs $ V.sum . V.zipWith (*) as <$> wvs
@@ -57,9 +60,9 @@ revaz xs = V.foldl' f (V.singleton xs, V.empty)
         av = V.head avs
 
 dCost :: (Num p, Ord p) => p -> p -> p
-dCost a y | y == 1 && a >= y = 0
-          | otherwise      = a - y
-
+dCost a y
+  | y == 1 && a >= y = 0
+  | otherwise = a - y
 
 vtranspose :: Vector (Vector a) -> Vector (Vector a)
 vtranspose (V.uncons -> Nothing) = V.empty
@@ -72,11 +75,11 @@ vtranspose v = ((V.fromList <$>) . V.fromList) . transpose . (V.toList <$>) . V.
 deltas :: Vector Double -> Vector Double -> NeuralNet -> (Vector (Vector Double), Vector (Vector Double))
 deltas xv yv vlayers = (V.reverse avs, f (vtranspose . snd <$> V.reverse vlayers) zvs (V.singleton delta0))
   where
-  (avs@(V.uncons -> Just (av, _)), V.uncons -> Just (zv, zvs)) = revaz xv vlayers
-  delta0 = V.zipWith (*) (V.zipWith dCost av yv) (relu' <$> zv)
-  f _ (V.uncons -> Nothing) dvs = dvs
-  f (V.uncons -> Just (wm, wms)) (V.uncons -> Just (zv, zvs)) dvs@(V.uncons -> Just (dv, _)) =
-    f wms zvs $ (`V.cons` dvs) $ V.zipWith (*) ((\row -> V.sum $ V.zipWith (*) row dv) <$> wm) (relu' <$> zv)
+    (avs@(V.uncons -> Just (av, _)), V.uncons -> Just (zv, zvs)) = revaz xv vlayers
+    delta0 = V.zipWith (*) (V.zipWith dCost av yv) (relu' <$> zv)
+    f _ (V.uncons -> Nothing) dvs = dvs
+    f (V.uncons -> Just (wm, wms)) (V.uncons -> Just (zv, zvs)) dvs@(V.uncons -> Just (dv, _)) =
+      f wms zvs $ (`V.cons` dvs) $ V.zipWith (*) ((\row -> V.sum $ V.zipWith (*) row dv) <$> wm) (relu' <$> zv)
 
 eta :: Double
 eta = 0.002
@@ -109,37 +112,41 @@ render n = let s = " .:oO@" in s !! (fromIntegral n * length s `div` 256)
 
 main :: IO ()
 main = do
-  as                             <- getArgs
-  [trainI, trainL, testI, testL] <- mapM
-    ((decompress <$>) . BS.readFile)
-    [ "train-images-idx3-ubyte.gz" :: FilePath
-    , "train-labels-idx1-ubyte.gz"
-    , "t10k-images-idx3-ubyte.gz"
-    , "t10k-labels-idx1-ubyte.gz"
-    ]
+  as <- getArgs
+  [trainI, trainL, testI, testL] <-
+    mapM
+      ((decompress <$>) . BS.readFile)
+      [ "train-images-idx3-ubyte.gz" :: FilePath,
+        "train-labels-idx1-ubyte.gz",
+        "t10k-images-idx3-ubyte.gz",
+        "t10k-labels-idx1-ubyte.gz"
+      ]
 
   when (as == ["samplesjs"]) $ do
-    putStr $ unlines
-      [ "var samples = " ++ show (show . getImage testI <$> V.fromList [0 .. 49]) ++ ";"
-      , "function random_sample() {"
-      , "  return samples[Math.floor(Math.random() * samples.length)];"
-      , "}"
-      ]
+    putStr $
+      unlines
+        [ "var samples = " ++ show (show . getImage testI <$> V.fromList [0 .. 49]) ++ ";",
+          "function random_sample() {",
+          "  return samples[Math.floor(Math.random() * samples.length)];",
+          "}"
+        ]
     exitSuccess
 
   hSetBuffering stderr LineBuffering
   let (pStr, pStrLn) = case as of
         ["print"] -> (hPutStr stderr, hPutStrLn stderr)
-        _         -> (putStr, putStrLn)
+        _ -> (putStr, putStrLn)
 
   n <- (`mod` 10000) <$> randomIO
   pStr . unlines $ take 28 $ take 28 <$> iterate (drop 28) (V.toList (render <$> getImage testI n))
 
   b <- newBrain (V.fromList [784, 30, 10])
   let example = getX testI n
-      bs = V.scanl' (V.foldl' (\b n -> learn (getX trainI n) (getY trainL n) b))
-                 b
-                 (V.fromList (V.fromList <$> [[0 .. 999], [1000 .. 2999], [3000 .. 5999], [6000 .. 9999]]))
+      bs =
+        V.scanl'
+          (V.foldl' (\b n -> learn (getX trainI n) (getY trainL n) b))
+          b
+          (V.fromList (V.fromList <$> [[0 .. 999], [1000 .. 2999], [3000 .. 5999], [6000 .. 9999]]))
       smart = V.last bs
       cute d score = show d ++ ": " ++ replicate (round $ 70 * min 1 score) '+'
       bestOf = fst . maximumBy (comparing snd) . zip [0 ..]
@@ -154,4 +161,4 @@ main = do
 
   case as of
     ["print"] -> print smart
-    _         -> return ()
+    _ -> return ()
